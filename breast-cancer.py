8dc0dab37354987b21305fcfaa52dd3ad1c1b1ab
Dat Nguyen <westbrook210207@gmail.com>
2026-07-01 08:21:47 +0700

    add note

--- breast-cancer.py
+++ breast-cancer.py
@@ -13,6 +13,7 @@ y = le.fit_transform(y)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42, stratify = y)
 
+# Tree base models don't need feature scaling, so we can skip that step
 from xgboost import XGBClassifier
 classifier = XGBClassifier(n_estimator=200)
 classifier.fit(X_train, y_train)
